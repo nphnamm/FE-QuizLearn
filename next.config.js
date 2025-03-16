fb70e5a1139fb924862e147078a13e5da0eed21a
NamNPH <nphnamm@gmail.com>
2025-03-16 14:07:30 +0700

    update

--- next.config.js
+++ next.config.js
@@ -4,30 +4,21 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8000/api/:path*",
+        destination: "https://api-quizlearn.onrender.com/api/:path*",
       },
     ];
   },
   images: {
     domains: ["images.unsplash.com"],
   },
-  // Use a configuration that allows for both static and server components
-  output: "standalone",
+  // Use hybrid approach
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Custom webpack config to handle errors when copying files
-  webpack: (config, { isServer }) => {
-    // Ignore ENOENT errors when copying route group files
-    config.infrastructureLogging = {
-      level: 'error',
-    };
-    
-    return config;
-  },
+  poweredByHeader: false,
 };
 
 module.exports = nextConfig;
