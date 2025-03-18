4c653b6afb1857301a9d0a90e81f13ce12ec57b1
NamNPH <nphnamm@gmail.com>
2025-03-18 14:31:56 +0700

    update

--- redux/store.ts
+++ redux/store.ts
@@ -1,24 +1,51 @@
 "use client";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { apiSlice } from "./features/api/apiSlice";
 import authSlice from "./features/auth/authSlice";
 import layoutReducer from "./features/layout/layoutSlice";
-import { localStorageMiddleware, loadState } from "./middlewares/localStorage";
+import { 
+  persistStore, 
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-// Load preloaded state from localStorage
-const preloadedState = loadState();
-
-export const store = configureStore({
-  reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
-    auth: authSlice,
-    layout: layoutReducer,
-  },
-  preloadedState,
-  devTools: process.env.NODE_ENV !== 'production',
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware, localStorageMiddleware),
+// Define root reducer with all reducers
+const rootReducer = combineReducers({
+  [apiSlice.reducerPath]: apiSlice.reducer,
+  auth: authSlice,
+  layout: layoutReducer,
 });
+
+// Redux Persist config
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['layout'], // Only persist layout for now
+};
+
+// Create persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure store with persisted reducer
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(apiSlice.middleware),
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+// Create persister
+export const persistor = persistStore(store);
 
 const initializeApp = async () => {
   try {
--- src/providers/ReduxProvider.tsx
+++ src/providers/ReduxProvider.tsx
@@ -1,8 +1,36 @@
 "use client";
 
 import { Provider } from "react-redux";
-import { store } from "../../redux/store";
+import { store, persistor } from "../../redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
+import Loader from "@/components/loader/Loader";
+import { useEffect, useState } from "react";
+
+// This component ensures we don't try to access localStorage during SSR
+function SafePersistGate({ children }: { children: React.ReactNode }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return <Loader />;
+  }
+
+  return (
+    <PersistGate loading={<Loader />} persistor={persistor}>
+      {children}
+    </PersistGate>
+  );
+}
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <SafePersistGate>
+        {children}
+      </SafePersistGate>
+    </Provider>
+  );
 }
